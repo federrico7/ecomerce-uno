317020ff5c9b2dee6e2a663953c8d1912cedcf93
Federico <espinosa_816@hotmail.com>
2022-09-27 21:17:15 -0300

    agregados

--- my-app/src/components/CenterPage.js
+++ my-app/src/components/CenterPage.js
@@ -3,10 +3,11 @@ import './CenterPage.css';
 
 export const CenterPage = () => {
 
-    return <content>
+    return 
+    <>
         <video class="bg-video" src="https://www.norton.com.ar/wp-content/uploads/2018/01/norton-brand-1.mp4" 
         poster="https://www.norton.com.ar/wp-content/uploads/2018/01/bg-home-slide.jpg" 
         width="100%" height="100%" autoplay="" loop="" preload="auto"></video>
-    </content>
-
+    
+    </>
 }
